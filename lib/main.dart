da6a68eee7b106a212bf7ecde7018601af703cd7
Anthony Rozon <arozonm@gmail.com>
2019-11-10 08:39:26 -0400

    added basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,56 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+    print('Answer choosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var _questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+      'What\'s your favorite city?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Text('Body Text'),
+        body: Column(
+          children: <Widget>[
+            Question(_questions[_questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: _answerQuestion,
+            )
+          ],
+        ),
       ),
     );
   }
